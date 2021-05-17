c4827dfa5147cf92e76718450da09cbd73d080d7
AnGel-91 <adrian991999@wp.pl>
2021-05-17 17:43:14 +0200

    add map to contact page

---
+++ client/src/components/template/map/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useToggleClick } from "_hooks";
+import {Link} from 'react-router-dom';
+import "./style/map.scss";
+
+const center = {
+  lat: 52.237049,
+  lng: 21.017532,
+};
+
+export const Map: FC = () => {
+  const { open, handleToggle } = useToggleClick();
+  const position = { lat: 52.17049, lng: 21.0532 };
+  const infoWindow = <div className="map__info">
+      <h3 className='map__title'>Furniture Shop</h3>
+      <p className='map__description'>visit us in our store</p>
+      <Link to='/' className='map__link'>Let's see</Link>
+  </div>;
+
+  return (
+    <>
+      <div className="map">
+        <LoadScript googleMapsApiKey="AIzaSyBaZVP2de7RVAqYzez-QHejA3ogG4E8jLQ">
+          <GoogleMap
+            mapContainerClassName="map__wrapper"
+            center={center}
+            zoom={10}
+          >
+            <Marker {...{ position, onClick: handleToggle }} />
+          </GoogleMap>
+        </LoadScript>
+        {open && infoWindow}
+      </div>
+    </>
+  );
+};
--- client/src/components/template/map/map.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-
-export const Map: FC = () => {
-  return <section style={{ height: '500px' }}>Mapa</section>;
-};
