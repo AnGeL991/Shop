695fce64b3aecf9143981cae23f4224241779404
AnGel-91 <adrian991999@wp.pl>
2021-05-22 20:23:30 +0200

    add images

--- client/src/components/template/sale/index.tsx
+++ client/src/components/template/sale/index.tsx
@@ -9,7 +9,7 @@ export const Sale: FC<ISale> = ({ discount = 35 }) => {
   return (
     <section className="sale">
       <article className="sale__product">
-        <img src="http://wordpress.templatemela.com/woo/WCM05/WCM050119/wp-content/uploads/2019/08/right-banner-2.jpg?fbclid=IwAR34vlKD3hJdscUztUnGxbVYkacClhbX5xvTE7QfmB8ejVQb76p9WxQRGq4" alt='saleFoto' />
+        <img src='./images/right-banner.jpg' alt='saleFoto' />
       </article>
       <article className="sale__discount">
         <h3 className="sale__discountTitle">Sale</h3>
--- client/src/db/index.ts
+++ client/src/db/index.ts
@@ -311,18 +311,15 @@ export const mainBaner = [
 ];
 export const fotosGalery = [
   {
-    image:
-      "http://wordpress.templatemela.com/woo/WCM05/WCM050119/wp-content/uploads/2019/08/9.jpg",
+    image: "./images/9.jpg",
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
   },
   {
-    image:
-      "http://wordpress.templatemela.com/woo/WCM05/WCM050119/wp-content/uploads/2019/08/10.jpg",
+    image: "./images/10.jpg",
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
   },
   {
-    image:
-      "http://wordpress.templatemela.com/woo/WCM05/WCM050119/wp-content/uploads/2019/08/11.jpg",
+    image: "./images/11.jpg",
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
   },
   {
